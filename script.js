28e27f2d04f1cd7f90e092b7b4dc7e60067fffa7
Aamir <aamirasaram2107@gmail.com>
2023-05-18 13:29:06 +0200

    Add form validation

--- script.js
+++ script.js
@@ -208,3 +208,58 @@ window.addEventListener('resize', () => {
   document.body.removeChild(bg);
   createPopup(globalID);
 });
+
+//form validation
+const form = document.querySelector('.form');
+const email = document.getElementById('email');
+const emailRegex = /^[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+const error = document.getElementById('error');
+error.className = "error";
+
+window.addEventListener('load', () => {
+  // Here, we test if the field is empty (remember, the field is not required)
+  // If it is not, we check if its content is a well-formed email address.
+  const isValid = email.value.length !== 0 || emailRegex.test(email.value);
+  email.className = isValid ? "valid" : "invalid";
+});
+
+// This defines what happens when the user types in the field
+email.addEventListener('input', () => {
+  const isValid = email.value.length !== 0 && emailRegex.test(email.value);
+  if (isValid) {
+    email.className = "valid";
+    error.textContent = "";
+    error.className = "error";
+  } else {
+    email.className = "invalid";
+    if (email.value.toLowerCase() !== email.value) {
+      error.textContent = "Email address should be lowercase, try " + email.value.toLowerCase();
+    } else {
+      error.textContent = "Please enter a valid email address";
+    }
+  }
+});
+
+// This defines what happens when the user tries to submit the data
+form.addEventListener('submit', (event) => {
+
+
+  const isValid = email.value.length !== 0 && emailRegex.test(email.value);
+  if (!isValid) {
+    email.className = "invalid";
+    //check if email is lowercase
+    if (email.value.toLowerCase() !== email.value) {
+      error.textContent = "Email address should be lowercase, try " + email.value.toLowerCase();
+    } else {
+      error.textContent = "Please enter a valid email address";
+    }
+    event.preventDefault();
+
+    error.className = "error active";
+  } else {
+    email.className = "valid";
+    error.textContent = "";
+    error.className = "error";
+    form.requestSubmit();
+  }
+});
